2a507411e5215ddd4beed90e1106d2e860958190
WajidKagzi <wjkagzi@gmail.com>
2016-04-10 10:30:15 -0400

    Add styling to search page

--- SearchPage.js
+++ SearchPage.js
@@ -11,7 +11,7 @@ var {
   Image,
   Component
 } = React;
-var styles = Stylesheet.create({
+var styles = StyleSheet.create({
   description: {
     marginBottom: 20,
     fontSize: 18,
@@ -22,6 +22,39 @@ var styles = Stylesheet.create({
     padding: 30,
     marginTop: 65,
     alignItems: 'center'
+  },
+  flowRight: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'stretch'
+  },
+  button: {
+    height: 36,
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center'
+  },
+  searchInput: {
+    height: 36,
+    padding: 4,
+    marginRight: 5,
+    flex: 4,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#48BBEC',
+    borderRadius: 8,
+    color: '#48BBEC'
   }
 });
 
@@ -35,6 +68,15 @@ class SearchPage extends Component {
         <Text style={styles.description}>
           Search by place-name, postcode, or search near your location.
         </Text>
+        <View style={styles.flowRight}>
+          <TextInput style={styles.searchInput} placeholder='Search via name or postcode'/>
+          <TouchableHighlight style={styles.button} underlayColor='#99d9f4'>
+            <Text style={styles.buttonText}>Go</Text>
+          </TouchableHighlight>
+        </View>
+        <TouchableHighlight style={styles.button} underlayColor='#99d9f4'>
+          <Text style={styles.buttonText}>Location</Text>
+        </TouchableHighlight>
       </View>
     );
   }
