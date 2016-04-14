bbb77065b21a451ba5fa217b5231bbf6fb91620c
WajidKagzi <wjkagzi@gmail.com>
2016-04-13 23:52:56 -0400

    Work on bug for search

--- SearchPage.js
+++ SearchPage.js
@@ -62,6 +62,25 @@ var styles = StyleSheet.create({
   }
 });
 
+function urlForQueryAndPage(key, value, pageNumber) {
+  var data = {
+    country: 'uk',
+    pretty: '1',
+    encoding: 'json',
+    listing_type: 'buy',
+    action: 'search_listings',
+    page: pageNumber
+  };
+
+  data[key] = value;
+
+  var queryString = Object.keys(data)
+    .map(key => key + '=' + encodeURIComponent(data[key]))
+    .join('&');
+
+  return 'http://api.nestoria.co.uk/api?' + queryString;
+};
+
 class SearchPage extends Component {
   constructor(props) {
     super(props);
@@ -75,6 +94,15 @@ class SearchPage extends Component {
     this.setState({ searchString: event.nativeEvent.text });
   }
 
+  _executeQuery(query) {
+    this.setState({ isLoading: true });
+  }
+
+  onSearchPressed() {
+    var query = urlForQueryAndPage('placename', this.state.searchString, 1);
+    this._executeQuery(query);
+  }
+
   render() {
     var spinner = this.state.isLoading ? (< ActivityIndicatorOS size='large'/>) : (<View />);
     return (
@@ -86,8 +114,12 @@ class SearchPage extends Component {
           Search by place-name, postcode, or search near your location.
         </Text>
         <View style={styles.flowRight}>
-          <TextInput style={styles.searchInput} value={this.state.searchString} onChange={this.onSearchTextChanged.bind(this)} placeholder='Search via name or postcode'/>
-          <TouchableHighlight style={styles.button} underlayColor='#99d9f4'>
+          <TextInput
+            style={styles.searchInput}
+            value={this.state.searchString}
+            onChange={this.onSearchTextChanged.bind(this)}
+            placeholder='Search via name or postcode'/>
+          <TouchableHighlight style={styles.button} underlayColor='#99d9f4' onPress={this.onSearchPressed.bind(this)}>
             <Text style={styles.buttonText}>Go</Text>
           </TouchableHighlight>
         </View>
